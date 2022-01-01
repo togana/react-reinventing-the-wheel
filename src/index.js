04e6903ae0e1c943a8c063ec4be2e5d00c05d38c
togana <o.togana@gmail.com>
2022-01-01 17:11:13 +0900

    ファイバーツリーの実装

--- src/index.js
+++ src/index.js
@@ -38,15 +38,74 @@ function createDom(fiber) {
   return dom
 }
 
+let nextUnitOfWork = null
+
 function render(element, container) {
-  const dom = createDom(element)
-
-  element.props.children.forEach(child =>
-    render(child, dom)
-  )
-
-  container.appendChild(dom)
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  }
 }
+
+function workLoop(deadline) {
+  let shouldYield = false
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(
+      nextUnitOfWork
+    )
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  requestIdleCallback(workLoop)
+}
+
+function performUnitOfWork(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom)
+  }
+
+  const elements = fiber.props.children
+  let index = 0
+  let prevSibling = null
+
+  while (index < elements.length) {
+    const element = elements[index]
+
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    }
+
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevSibling.sibling = newFiber
+    }
+
+    prevSibling = newFiber
+    index++
+  }
+
+  if (fiber.child) {
+    return fiber.child
+  }
+  let nextFiber = fiber
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling
+    }
+    nextFiber = nextFiber.parent
+  }
+}
+
+requestIdleCallback(workLoop)
 
 const Didact = {
   createElement,
@@ -55,9 +114,12 @@ const Didact = {
 
 /** @jsx Didact.createElement */
 const element = (
-  <div id="foo">
-    <a>bar</a>
-    <b />
+  <div>
+    <h1>
+      <p />
+      <a>bar</a>
+    </h1>
+    <h2 />
   </div>
 )
 const container = document.getElementById("root")
