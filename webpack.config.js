f5d306f4e928bc8295eaaf2625db0e8e4b29fa7a
togana <o.togana@gmail.com>
2022-01-01 14:44:29 +0900

    babel の設定追加した

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,23 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+            ]
+          }
+        }
+      }
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'react-reinventing-the-wheel',
